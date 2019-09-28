e3cf1c269b1b3f3a4960b717f4e628db703d7733
Mike Lirio <mliriovi@everis.com>
2019-09-28 21:08:25 +0100

    Added a loop until we read a file.

--- src/main/java/oca/file/reader/ReadingFiles.java
+++ src/main/java/oca/file/reader/ReadingFiles.java
@@ -2,14 +2,31 @@ package oca.file.reader;
 
 import java.util.logging.Logger;
 import java.util.logging.Level;
-import oca.constants.FileContansts;
-
 import java.io.*;
+import oca.constants.FileContansts;
 
 public class ReadingFiles {
     public static void main(String [] args) {
+        BufferedReader keyboard = new BufferedReader(new InputStreamReader(System.in));
+
+        Boolean success = false;
+        while(!success) {
+            Logger.getLogger(ReadingFiles.class.getName())
+                .log(Level.INFO, "Write a file to read on " + FileContansts.READ_FOLDER_EVERIS + ": ");
+            try {
+                String fileName = keyboard.readLine();
+                success = processFile(fileName);
+            } catch (IOException e) {
+                String errorMsg = "An error appears when it try to read from keyboard.";
+                Logger.getLogger(ReadingFiles.class.getName())
+                    .log(Level.SEVERE, errorMsg, e);
+            }
+        }
+    }
+
+    private static Boolean processFile(String fileName) {
         try {
-            FileReader reader = new FileReader(FileContansts.READ_FOLDER + "readingFileExample.txt");
+            FileReader reader = new FileReader(FileContansts.READ_FOLDER_EVERIS + fileName);
             BufferedReader buffer = new BufferedReader(reader);
 
             StringBuilder builder = new StringBuilder();
@@ -19,14 +36,19 @@ public class ReadingFiles {
             }
 
             System.out.println("File content: " + builder);
+            Logger.getLogger(ReadingFiles.class.getName())
+                .log(Level.INFO, "File content: " + builder);
+            return true;
         } catch (FileNotFoundException e) {
-            String errorMsg = "An error appers when it was trying to read the file";
+            String errorMsg = "File " + fileName + " do not found it.";
             Logger.getLogger(ReadingFiles.class.getName())
                 .log(Level.SEVERE, errorMsg, e);
+            return false;
         } catch (IOException e) {
             String errorMsg = "An error appers when it was trying to read the file";
             Logger.getLogger(ReadingFiles.class.getName())
                 .log(Level.SEVERE, errorMsg, e);
+            return false;
         }
     }
 }
