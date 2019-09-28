c17b12a766db08d269ef287abefe5702782b4312
Mike Lirio <mliriovi@everis.com>
2019-09-28 20:43:42 +0100

    Added error handling

--- src/main/java/oca/file/reader/ReadingFiles.java
+++ src/main/java/oca/file/reader/ReadingFiles.java
@@ -1,20 +1,32 @@
 package oca.file.reader;
 
+import java.util.logging.Logger;
+import java.util.logging.Level;
 import oca.constants.FileContansts;
 
 import java.io.*;
 
 public class ReadingFiles {
-    public static void main(String [] args) throws Throwable {
-        FileReader reader = new FileReader(FileContansts.READ_FOLDER + "readingFileExample.txt");
-        BufferedReader buffer = new BufferedReader(reader);
+    public static void main(String [] args) {
+        try {
+            FileReader reader = new FileReader(FileContansts.READ_FOLDER + "readingFileExample.txt");
+            BufferedReader buffer = new BufferedReader(reader);
 
-        StringBuilder builder = new StringBuilder();
-        String line = null;
-        while ((line = buffer.readLine()) != null) {
-            builder.append(line + "\n");
+            StringBuilder builder = new StringBuilder();
+            String line = null;
+            while ((line = buffer.readLine()) != null) {
+                builder.append(line + "\n");
+            }
+
+            System.out.println("File content: " + builder);
+        } catch (FileNotFoundException e) {
+            String errorMsg = "An error appers when it was trying to read the file";
+            Logger.getLogger(ReadingFiles.class.getName())
+                .log(Level.SEVERE, errorMsg, e);
+        } catch (IOException e) {
+            String errorMsg = "An error appers when it was trying to read the file";
+            Logger.getLogger(ReadingFiles.class.getName())
+                .log(Level.SEVERE, errorMsg, e);
         }
-
-        System.out.println("File content: " + builder);
     }
 }
