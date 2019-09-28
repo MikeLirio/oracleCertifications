d55949918d382c4ef87fee637b9eb47d1adb8383
Mike Lirio <mliriovi@everis.com>
2019-09-28 21:08:07 +0100

    Added Constants to locale the files depending of the computer I'm using

--- src/main/java/oca/constants/FileContansts.java
+++ src/main/java/oca/constants/FileContansts.java
@@ -1,5 +1,6 @@
 package oca.constants;
 
 public class FileContansts {
-    public static String READ_FOLDER = "D:\\Desarrollo\\certificates\\oracle\\readFolder\\";
+    public static String READ_FOLDER_MIKE = "C:\\certificate\\certificates\\oracle\\readFolder\\";
+    public static String READ_FOLDER_EVERIS = "C:\\certificate\\";
 }
