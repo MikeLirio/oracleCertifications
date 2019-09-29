4c4fb5737901209efcddf1cdca03dd3c639cabb7
Mike Lirio <mliriovi@everis.com>
2019-09-29 19:12:45 +0100

    Added Forms with Intellij. Quite boring.

---
+++ src/main/java/oca/user/gui/TestGUIForm.java
@@ -0,0 +1,10 @@
+package oca.user.gui;
+
+import javax.swing.JButton;
+import javax.swing.JTextField;
+
+public class TestGUIForm {
+
+    private JButton clickMeButton;
+    private JTextField textField1;
+}
